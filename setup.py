1b80e58065970095082bb0beb58bb87d8f5033dd
tahaafarooq <tahacodez@gmail.com>
2023-06-14 23:44:16 +0300

    Created a setup.py file for installation

---
+++ setup.py
@@ -0,0 +1,34 @@
+from os import path
+from setuptools import setup
+
+this_directory = path.abspath(path.dirname(__file__))
+with open(path.join(this_directory, "README.md"), encoding="utf-8") as f:
+    long_description = f.read()
+
+setup(
+    name="plutus",
+    version="1.0",
+    description="Plutus is an API that is made ready for the purpose of monitoring your server's processes and identifying strange processes!",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/tahaafarooq/plutus",
+    download_url="",
+    author="Tahaa Farooq",
+    author_email="tahacodez@gmail.com",
+    license="MIT",
+    packages=["plutus"],
+    keywords=[
+        "Blue Teaming",
+        "monitoring tool" "monitoring" "cybersecurity" "blueteaming" "security tools",
+        "python-tanzania",
+    ],
+    install_requires=["libmagic", "python-magic-bin==0.4.14", "requests"],
+    include_package_data=True,
+    python_requires=">=3.7",
+    classifiers=[
+        "Intended Audience :: Security Researchers",
+        "Topic :: Cyber Security :: Build Tools",
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3.10",
+    ],
+)
