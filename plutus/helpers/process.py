de70725da57051c4f6308e0b14e7b0a9f03d027c
tahaafarooq <tahacodez@gmail.com>
2023-06-18 23:02:00 +0300

    Added a function to monitor systemd services

--- plutus/helpers/process.py
+++ plutus/helpers/process.py
@@ -1,10 +1,20 @@
 import psutil
+import systemd
 
 
 class MonitorProcess(object):
     def __init__(self):
         self.__description__ = "This is a monitoring class"
 
+    # get all systemd services running
+
+    def get_all_services(self):
+        manager = systemd.Manager()
+        services = manager.list_units()
+
+        return [service.UnitName for service in services if services.Type == 'service']
+
+    # monitor the cronjob processes and dump
     @staticmethod
     def monitor_cronjobs():
         cron_processes = []
@@ -26,3 +36,23 @@ class MonitorProcess(object):
             return output
         else:
             return "NO CRONJOB FOUND"
+
+    # monitor for all systemd services running
+    def monitor_systemd(self):
+        services = self.get_all_services()
+        output = []
+        errors = []
+
+        for service_name in services:
+            try:
+                unit = systemd.Unit(service_name)
+                unit.load()
+                unit_state = unit.Unit.ActiveState
+                
+                data = f"[SERVICE] [{service_name}] : [SERVICE STATE] [{unit_state}]"
+                output.append(data)
+            except systemd.exceptions.SystemdException as e:
+                data = f"ERROR RETRIEVING STATUS FOR SERVICE [{service_name}]"
+                errors.append(data)
+
+        return output
